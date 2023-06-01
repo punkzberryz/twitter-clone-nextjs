40849ed0a14dab657773c771a977b6f0e9ff645f
punkzberryz <16286841+punkzberryz@users.noreply.github.com>
2023-06-01 17:06:26 +0800

    update tweet form

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
 import Head from "next/head";
+import SideNav from "~/components/SideNav";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -16,9 +17,9 @@ const MyApp: AppType<{ session: Session | null }> = ({
         <meta name="description" content="This is a Twitter Clone" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="container mx-auto flex">
-        {/* <SideNav /> */}
-        <div className="">
+      <div className="container mx-auto flex items-start sm:pr-4">
+        <SideNav />
+        <div className="min-h-screen flex-grow border-x">
           <Component {...pageProps} />
         </div>
       </div>
